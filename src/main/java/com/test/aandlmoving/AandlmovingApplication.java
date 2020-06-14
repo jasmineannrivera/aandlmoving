38963ac4163f63d0c903c53ed0b77af6954ab38a
Jasmine Rivera <jasmineannrivera@gmail.com>
2020-06-13 22:42:18 -0500

    created new repo and some files

---
+++ src/main/java/com/test/aandlmoving/AandlmovingApplication.java
@@ -0,0 +1,13 @@
+package com.test.aandlmoving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AandlmovingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AandlmovingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/aandlmoving/AandlmovingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.aandlmoving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AandlmovingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
